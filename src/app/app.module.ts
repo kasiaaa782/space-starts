4ae0bdbef90df340c42c3d1dac1b48449f1f0d7a
Katarzyna Niemiec <katarzyna.niemiec.programista@gmail.com>
2022-05-10 22:33:22 +0200

    Added DatePicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { SharedComponentsModule } from 'src/shared/commons/components/shared-components.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartsExploreComponent } from './starts-explore/starts-explore.component';
@@ -9,7 +12,14 @@ import { StartsFiltersComponent } from './starts-explore/starts-filters/starts-f
 
 @NgModule({
   declarations: [AppComponent, StartsExploreComponent, StartsFiltersComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot(),
+    SharedComponentsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
